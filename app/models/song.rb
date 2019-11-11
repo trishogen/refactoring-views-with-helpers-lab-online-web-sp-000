cef72d331c7d2be5fcdea75fbeb61df009e9818d
Trish Ogen <patriciaogen@gmail.com>
2019-11-11 14:09:24 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,10 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def artist_name
+    self.artist ? self.artist.name : 'Unknown Artist'
   end
 
   def artist_name=(name)
+    self.artist.name = name
   end
 end
